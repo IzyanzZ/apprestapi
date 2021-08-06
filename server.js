6ad01cd5d574e40bff479f22ad96026b79505b80
IzyanzZ <izyanzytbusiness@gmail.com>
2021-08-06 18:14:04 +0700

    setting routes di server.js

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const app = express()
 const bodyParser = require('body-parser')
 
 
-
+// BodyParser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Console Log (Port Nya 3000)
 app.listen(3000, () => {
     console.log(`Server started on 300`);
 });
+
+// Setting Routes
+const routes = require('./router')
 routes(app)

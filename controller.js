b4f5797f0fcd73f39b28336d57b4d38b7cdff579
IzyanzZ <izyanzytbusiness@gmail.com>
2021-08-06 18:28:14 +0700

    Memulai Dari Awal

--- controller.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-var respone = require('./res')
-var connection = require('./koneksi')
-
-exports.index = function(req,  res){
-    respone.ok("Aplikasi REST API Ku berjalan!",res)
-}
